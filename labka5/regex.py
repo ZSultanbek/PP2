67f76dd52c917e1692a00375a5962d8a8f3e8f8d
ZSultanbek <144799200+ZSultanbek@users.noreply.github.com>
2024-02-28 21:17:44 +0600

    Add files via upload

---
+++ labka5/regex.py
@@ -0,0 +1,69 @@
+import re
+text = open(r"labka5\raw.txt", encoding="utf8")
+txt = " ".join((str(text.read())).split("\n"))
+
+
+#1________________________________
+print("1______________________")
+abb1 = re.findall("ab*", txt)
+print(abb1)
+
+#2________________________________
+print("2______________________")
+abb2 = re.findall("a{1}b{2,3}", txt)
+print(abb2)
+
+
+#3________________________________
+print("3______________________")
+lowunder = re.findall(r"^[a-z]+_[a-z]+$", txt)
+print(lowunder)
+
+
+#4________________________________
+print("4______________________")
+upplow = re.findall("[A-Z][a-z]+", txt)
+print(upplow)
+
+#5________________________________
+print("5______________________")
+a_b = re.findall(r"a.*b$", txt)
+print(a_b)
+
+#6________________________________
+print("6______________________")
+def ReplaceWithColon(input_string):
+    return re.sub("[ ,.]", ":", input_string)
+ReplaceWithColon(txt)
+print(txt)
+
+#7________________________________
+print("7______________________")
+def Snake_case_toCamelCase(snake_case):
+    words = snake_case.split('_')
+    return words[0] + ''.join(word.capitalize() for word in words[1:])
+Snake_case_toCamelCase(txt)
+print(txt)
+
+#8________________________________
+print("8______________________")
+def SplitAtUppercase(input_string):
+    return re.split(r'(?=[A-Z])', input_string)
+SplitAtUppercase(txt)
+print(txt)
+
+#9________________________________
+print("9______________________")
+def InsertSpacesAtCaps(input_string):
+    return re.sub(r'(?<=[a-z])(?=[A-Z])', ' ', input_string)
+InsertSpacesAtCaps(txt)
+print(txt)
+
+#10_______________________________
+print("10______________________")
+def CamelCase_to_snake_case(camel_case):
+    return re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', camel_case).lower()
+CamelCase_to_snake_case(txt)
+
+text.close()
+print(txt)
